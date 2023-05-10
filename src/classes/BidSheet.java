8eeebb30215ab3b1589909cd79584ff860faca97
RyanBoston27 <ryanboston63@gmail.com>
2023-05-09 20:21:35 -0500

    Forgot some data fields for bid sheet

--- src/classes/BidSheet.java
+++ src/classes/BidSheet.java
@@ -1,10 +1,17 @@
 package classes;
 public class BidSheet {
+    
     // private data members
+    // Each member corresponds to a data item in the bid sheet
     private CustomerInfo customerInfo;
     private WaterService waterService;
     private SewerService sewerService;
     private LiningService liningService;
+    private boolean furnishPermits;
+    private boolean breakUpFloor;
+    private boolean recementFloor;
+    private boolean trafficControl;
+    private String explain; 
 
     // Default constructor
     public BidSheet() {
@@ -31,6 +38,26 @@ public class BidSheet {
         return this.liningService;
     }
 
+    public boolean isFurnishPermits() {
+        return this.furnishPermits;
+    }
+    
+    public boolean isBreakUpFloor() {
+        return this.breakUpFloor;
+    }
+    
+    public boolean isRecementFloor() {
+        return this.recementFloor;
+    }
+    
+    public boolean isTrafficControl() {
+        return this.trafficControl;
+    }
+    
+    public String getExplain() {
+        return this.explain;
+    }
+
     // Setters
     public void setCustomerInfo(CustomerInfo customerInfo) {
         this.customerInfo = customerInfo;
@@ -46,5 +73,25 @@ public class BidSheet {
 
     public void setLiningService(LiningService liningService) {
         this.liningService = liningService;
+    }
+
+    public void setFurnishPermits(boolean furnishPermits) {
+        this.furnishPermits = furnishPermits;
+    }
+    
+    public void setBreakUpFloor(boolean breakUpFloor) {
+        this.breakUpFloor = breakUpFloor;
+    }
+    
+    public void setRecementFloor(boolean recementFloor) {
+        this.recementFloor = recementFloor;
+    }
+    
+    public void setTrafficControl(boolean trafficControl) {
+        this.trafficControl = trafficControl;
+    }
+    
+    public void setExplain(String explain) {
+        this.explain = explain;
     }
 }
