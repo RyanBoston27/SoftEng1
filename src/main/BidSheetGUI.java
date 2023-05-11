135712a2a9cdd7b96280ea5e293a468e35534367
tmicheal <Charnecke1@gmail.com>
2023-05-10 23:28:32 -0500

    Update BidSheetGUI.java

--- src/main/BidSheetGUI.java
+++ src/main/BidSheetGUI.java
@@ -3,19 +3,34 @@ package main;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JRadioButton;
-import java.awt.BorderLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JLabel;
-import javax.swing.JScrollPane;
-import java.awt.GridLayout;
-import javax.swing.JButton;
-import javax.swing.JToggleButton;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JRadioButton;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class BidSheetGUI {
 
 	private JFrame frame;
-	private JTextField textField;
+	private JLabel lblPropertyLine2;
+	private JTextField txtName;
+	private JTextField txtEmail;
+	private JTextField txtJobAdd;
+	private JTextField txtDate;
+	private JTextField txtPhone;
+	private JTextField txtBillAdd;
+	private JTextField txtAddFt;
+	private JTextField txtAppFt;
 
 	/**
 	 * Launch the application.
@@ -45,29 +60,324 @@ public class BidSheetGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 611);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel label = new JLabel("");
-		label.setBounds(145, 130, 144, 130);
-		frame.getContentPane().add(label);
+		JLabel lblCusName = new JLabel("Customer Name:  ");
+		lblCusName.setBounds(6, 6, 86, 14);
+		frame.getContentPane().add(lblCusName);
 		
-		JLabel label_1 = new JLabel("");
-		label_1.setBounds(289, 130, 144, 130);
-		frame.getContentPane().add(label_1);
+		JLabel lblDate = new JLabel("Date:");
+		lblDate.setBounds(271, 6, 27, 14);
+		frame.getContentPane().add(lblDate);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(60, 87, 89, 23);
-		frame.getContentPane().add(btnNewButton);
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setBounds(6, 26, 86, 14);
+		frame.getContentPane().add(lblEmail);
 		
-		JToggleButton tglbtnNewToggleButton = new JToggleButton("New toggle button");
-		tglbtnNewToggleButton.setBounds(56, 144, 121, 23);
-		frame.getContentPane().add(tglbtnNewToggleButton);
+		JLabel lblNewLabel_4 = new JLabel("Phone:");
+		lblNewLabel_4.setBounds(264, 26, 34, 14);
+		frame.getContentPane().add(lblNewLabel_4);
 		
-		textField = new JTextField();
-		textField.setBounds(230, 48, 86, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		JLabel lblJobAdd = new JLabel("Job Site Address:");
+		lblJobAdd.setBounds(6, 46, 86, 14);
+		frame.getContentPane().add(lblJobAdd);
+		
+		JLabel lblBillAdd = new JLabel("Billing Address:");
+		lblBillAdd.setBounds(6, 66, 86, 14);
+		frame.getContentPane().add(lblBillAdd);
+		
+		JLabel lblDesc = new JLabel("Description of services to be performed:");
+		lblDesc.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblDesc.setBounds(6, 86, 234, 14);
+		frame.getContentPane().add(lblDesc);
+		
+		JSeparator separator1 = new JSeparator();
+		separator1.setBounds(0, 106, 435, 2);
+		frame.getContentPane().add(separator1);
+		
+		JLabel lblWaterServ = new JLabel("1. Water Service:");
+		lblWaterServ.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblWaterServ.setBounds(6, 114, 109, 14);
+		frame.getContentPane().add(lblWaterServ);
+		
+		JLabel lblSize = new JLabel("Size:");
+		lblSize.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSize.setBounds(125, 114, 28, 14);
+		frame.getContentPane().add(lblSize);
+		
+		JLabel lblApproxFt1 = new JLabel("Approx. feet:  ");
+		lblApproxFt1.setBounds(212, 114, 86, 14);
+		frame.getContentPane().add(lblApproxFt1);
+		
+		JLabel lblDrop = new JLabel("Drop:  ");
+		lblDrop.setBounds(365, 114, 34, 14);
+		frame.getContentPane().add(lblDrop);
+		
+		JLabel lblBackFlow = new JLabel("Back flow:");
+		lblBackFlow.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblBackFlow.setBounds(16, 139, 69, 14);
+		frame.getContentPane().add(lblBackFlow);
+		
+		JLabel lblPropertyLine = new JLabel("Property Line:");
+		lblPropertyLine.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblPropertyLine.setBounds(4, 163, 81, 14);
+		frame.getContentPane().add(lblPropertyLine);
+		
+		JLabel lblAddHorFt = new JLabel("Additional horizontal Ft:");
+		lblAddHorFt.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblAddHorFt.setBounds(209, 139, 134, 14);
+		frame.getContentPane().add(lblAddHorFt);
+		
+		JLabel lblBlvd = new JLabel("Blvd:");
+		lblBlvd.setBounds(233, 163, 27, 14);
+		frame.getContentPane().add(lblBlvd);
+		
+		JLabel lblCityMain = new JLabel("City main:");
+		lblCityMain.setBounds(295, 163, 48, 14);
+		frame.getContentPane().add(lblCityMain);
+		
+		JLabel lblSewerService = new JLabel("2: Sewer Service: ");
+		lblSewerService.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblSewerService.setBounds(10, 190, 105, 14);
+		frame.getContentPane().add(lblSewerService);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setBounds(0, 188, 435, 2);
+		frame.getContentPane().add(separator_2);
+		
+		JLabel lblSize2 = new JLabel("Size:");
+		lblSize2.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSize2.setBounds(94, 190, 59, 14);
+		frame.getContentPane().add(lblSize2);
+		
+		JLabel lblApproxFt2 = new JLabel("Approx. feet:");
+		lblApproxFt2.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblApproxFt2.setBounds(198, 190, 81, 14);
+		frame.getContentPane().add(lblApproxFt2);
+		
+		lblPropertyLine2 = new JLabel("Property Line:");
+		lblPropertyLine2.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblPropertyLine2.setBounds(72, 224, 81, 14);
+		frame.getContentPane().add(lblPropertyLine2);
+		
+		JLabel lblCityMain_1 = new JLabel("City main:");
+		lblCityMain_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblCityMain_1.setBounds(220, 215, 59, 14);
+		frame.getContentPane().add(lblCityMain_1);
+		
+		JSeparator separator2 = new JSeparator();
+		separator2.setBounds(0, 236, 435, 2);
+		frame.getContentPane().add(separator2);
+		
+		JLabel lblLiningService = new JLabel("3: lining Service:");
+		lblLiningService.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblLiningService.setBounds(10, 240, 105, 14);
+		frame.getContentPane().add(lblLiningService);
+		
+		JLabel lblSize3 = new JLabel("Size:");
+		lblSize3.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSize3.setBounds(115, 240, 59, 14);
+		frame.getContentPane().add(lblSize3);
+		
+		JLabel lblApproxFt3 = new JLabel("Approx. feet:  ");
+		lblApproxFt3.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblApproxFt3.setBounds(222, 240, 76, 14);
+		frame.getContentPane().add(lblApproxFt3);
+		
+		JLabel lblExcavate = new JLabel("Excavate:  ");
+		lblExcavate.setBounds(243, 265, 59, 14);
+		frame.getContentPane().add(lblExcavate);
+		
+		JLabel lblTranLiner = new JLabel("Transition Liner:");
+		lblTranLiner.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblTranLiner.setBounds(93, 275, 81, 14);
+		frame.getContentPane().add(lblTranLiner);
+		
+		JLabel lblFurnish = new JLabel("4. Furnish permits:");
+		lblFurnish.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblFurnish.setBounds(0, 292, 93, 14);
+		frame.getContentPane().add(lblFurnish);
+		
+		JLabel lblBreakUp = new JLabel("5.  Break-up floor:");
+		lblBreakUp.setBounds(6, 317, 99, 14);
+		frame.getContentPane().add(lblBreakUp);
+		
+		JLabel lblRecement = new JLabel("6. Recement floor: ");
+		lblRecement.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblRecement.setBounds(0, 345, 99, 14);
+		frame.getContentPane().add(lblRecement);
+		
+		JLabel lblTraffic = new JLabel("7.   Traffic Control:");
+		lblTraffic.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblTraffic.setBounds(-7, 370, 99, 14);
+		frame.getContentPane().add(lblTraffic);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(6, 421, 418, 107);
+		frame.getContentPane().add(textArea);
+		
+		JLabel lblRemarks = new JLabel("Remarks:");
+		lblRemarks.setBounds(10, 405, 46, 14);
+		frame.getContentPane().add(lblRemarks);
+		
+		JSeparator separator3 = new JSeparator();
+		separator3.setBounds(0, 394, 435, 2);
+		frame.getContentPane().add(separator3);
+		
+		JLabel lblExplain = new JLabel("Explain:");
+		lblExplain.setBounds(253, 290, 46, 14);
+		frame.getContentPane().add(lblExplain);
+		
+		txtName = new JTextField();
+		txtName.setBounds(88, 3, 173, 20);
+		frame.getContentPane().add(txtName);
+		txtName.setColumns(10);
+		
+		txtEmail = new JTextField();
+		txtEmail.setBounds(88, 23, 173, 20);
+		frame.getContentPane().add(txtEmail);
+		txtEmail.setColumns(10);
+		
+		txtJobAdd = new JTextField();
+		txtJobAdd.setBounds(88, 43, 336, 20);
+		frame.getContentPane().add(txtJobAdd);
+		txtJobAdd.setColumns(10);
+		
+		txtDate = new JTextField();
+		txtDate.setBounds(301, 3, 123, 20);
+		frame.getContentPane().add(txtDate);
+		txtDate.setColumns(10);
+		
+		txtPhone = new JTextField();
+		txtPhone.setBounds(301, 23, 123, 20);
+		frame.getContentPane().add(txtPhone);
+		txtPhone.setColumns(10);
+		
+		txtBillAdd = new JTextField();
+		txtBillAdd.setBounds(88, 63, 336, 20);
+		frame.getContentPane().add(txtBillAdd);
+		txtBillAdd.setColumns(10);
+		
+		JRadioButton rdbtn34 = new JRadioButton("3/4\"");
+		rdbtn34.setBounds(159, 114, 46, 14);
+		frame.getContentPane().add(rdbtn34);
+		
+		JRadioButton rdbtn1 = new JRadioButton("1\"");
+		rdbtn1.setBounds(159, 131, 46, 14);
+		frame.getContentPane().add(rdbtn1);
+		
+		JRadioButton rdbtn2 = new JRadioButton("2\"");
+		rdbtn2.setBounds(159, 148, 35, 14);
+		frame.getContentPane().add(rdbtn2);
+		
+		JCheckBox chckBackFlow = new JCheckBox("");
+		chckBackFlow.setHorizontalAlignment(SwingConstants.LEFT);
+		chckBackFlow.setBounds(91, 135, 27, 23);
+		frame.getContentPane().add(chckBackFlow);
+		
+		JCheckBox chckPropLine = new JCheckBox("");
+		chckPropLine.setBounds(91, 159, 97, 23);
+		frame.getContentPane().add(chckPropLine);
+		
+		JCheckBox chckBlvd = new JCheckBox("");
+		chckBlvd.setHorizontalAlignment(SwingConstants.LEFT);
+		chckBlvd.setBounds(263, 159, 27, 23);
+		frame.getContentPane().add(chckBlvd);
+		
+		JCheckBox chckDrop = new JCheckBox("");
+		chckDrop.setBounds(403, 110, 21, 23);
+		frame.getContentPane().add(chckDrop);
+		
+		txtAddFt = new JTextField();
+		txtAddFt.setBounds(348, 136, 86, 20);
+		frame.getContentPane().add(txtAddFt);
+		txtAddFt.setColumns(10);
+		
+		JTextArea textArea_1 = new JTextArea();
+		textArea_1.setBounds(283, 109, 72, 22);
+		frame.getContentPane().add(textArea_1);
+		
+		JCheckBox chckCityMain = new JCheckBox("");
+		chckCityMain.setBounds(348, 159, 27, 23);
+		frame.getContentPane().add(chckCityMain);
+		
+		JCheckBox chckFurnish = new JCheckBox("");
+		chckFurnish.setBounds(93, 288, 21, 23);
+		frame.getContentPane().add(chckFurnish);
+		
+		JCheckBox chckBreak = new JCheckBox("");
+		chckBreak.setBounds(93, 313, 22, 23);
+		frame.getContentPane().add(chckBreak);
+		
+		JCheckBox chckRece = new JCheckBox("");
+		chckRece.setBounds(93, 341, 97, 23);
+		frame.getContentPane().add(chckRece);
+		
+		JCheckBox chckTraffic = new JCheckBox("");
+		chckTraffic.setBounds(93, 366, 97, 23);
+		frame.getContentPane().add(chckTraffic);
+		
+		JRadioButton rdbtn4 = new JRadioButton("4\"");
+		rdbtn4.setBounds(159, 190, 48, 14);
+		frame.getContentPane().add(rdbtn4);
+		
+		JRadioButton rdbtn6 = new JRadioButton("6\"");
+		rdbtn6.setBounds(159, 207, 35, 14);
+		frame.getContentPane().add(rdbtn6);
+		
+		JTextArea textAppFt2 = new JTextArea();
+		textAppFt2.setBounds(289, 188, 72, 22);
+		frame.getContentPane().add(textAppFt2);
+		
+		JCheckBox chckCityMain2 = new JCheckBox("");
+		chckCityMain2.setBounds(283, 215, 27, 23);
+		frame.getContentPane().add(chckCityMain2);
+		
+		JCheckBox chckPropLine2 = new JCheckBox("");
+		chckPropLine2.setBounds(159, 220, 21, 18);
+		frame.getContentPane().add(chckPropLine2);
+		
+		JRadioButton rdbtn4_1 = new JRadioButton("4\"");
+		rdbtn4_1.setBounds(179, 240, 48, 14);
+		frame.getContentPane().add(rdbtn4_1);
+		
+		JRadioButton rdbtn6_1 = new JRadioButton("6\"");
+		rdbtn6_1.setBounds(179, 258, 35, 14);
+		frame.getContentPane().add(rdbtn6_1);
+		
+		JCheckBox chckTranLine2 = new JCheckBox("");
+		chckTranLine2.setBounds(179, 271, 97, 23);
+		frame.getContentPane().add(chckTranLine2);
+		
+		txtAppFt = new JTextField();
+		txtAppFt.setBounds(295, 237, 129, 20);
+		frame.getContentPane().add(txtAppFt);
+		txtAppFt.setColumns(10);
+		
+		JCheckBox chckExcavate = new JCheckBox("");
+		chckExcavate.setBounds(295, 261, 27, 23);
+		frame.getContentPane().add(chckExcavate);
+		
+		JCheckBox chckNoPack = new JCheckBox("No Packing Signs");
+		chckNoPack.setBounds(295, 286, 123, 23);
+		frame.getContentPane().add(chckNoPack);
+		
+		JCheckBox chckLaneSh = new JCheckBox("Lane Shift");
+		chckLaneSh.setBounds(295, 313, 97, 23);
+		frame.getContentPane().add(chckLaneSh);
+		
+		JCheckBox chcksDetour = new JCheckBox("Detour");
+		chcksDetour.setBounds(295, 341, 97, 23);
+		frame.getContentPane().add(chcksDetour);
+		
+		JButton btnSub = new JButton("Submit");
+		btnSub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSub.setBounds(159, 538, 117, 23);
+		frame.getContentPane().add(btnSub);
 	}
 }
