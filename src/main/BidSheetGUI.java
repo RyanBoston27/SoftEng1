9901104ae62ec356c412f8afb92d82de41b2b416
tmicheal <Charnecke1@gmail.com>
2023-05-10 20:59:12 -0500

    bakjlfjn;dsa

---
+++ src/main/BidSheetGUI.java
@@ -0,0 +1,73 @@
+package main;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JRadioButton;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JToggleButton;
+import javax.swing.JTextField;
+
+public class PleaseGod {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PleaseGod window = new PleaseGod();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PleaseGod() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel label = new JLabel("");
+		label.setBounds(145, 130, 144, 130);
+		frame.getContentPane().add(label);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setBounds(289, 130, 144, 130);
+		frame.getContentPane().add(label_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(60, 87, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		JToggleButton tglbtnNewToggleButton = new JToggleButton("New toggle button");
+		tglbtnNewToggleButton.setBounds(56, 144, 121, 23);
+		frame.getContentPane().add(tglbtnNewToggleButton);
+		
+		textField = new JTextField();
+		textField.setBounds(230, 48, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+	}
+}
